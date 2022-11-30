19eb3ff9bfe537f4cd700a7ecf6ca12ccb3a6ffc
Jacqueline Rogge <j.rogge@outlook.com>
2022-11-29 23:21:57 -0500

    Update librairie-generale.php
    Adition fonction object to array (pas utilisé) et remplacer fractions (0.5 to 1/2)

--- librairies/librairie-generale.php
+++ librairies/librairie-generale.php
@@ -16,7 +16,6 @@ function ajouteZeros($numValeur, $intLargeur) {
         die();*/
 
 
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Convertir un sous chaine en entier
 function convertitSousChaineEnEntier($strChaine, $intDepart, $intLongueur) {
@@ -48,5 +47,38 @@ function er($intEntier, $binExposant=true) {
     return filter_input(INPUT_GET, $strIDParam, FILTER_SANITIZE_SPECIAL_CHARS) .
            filter_input(INPUT_POST, $strIDParam, FILTER_SANITIZE_SPECIAL_CHARS);
  }   
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//object to array
+ function objectToArray($objJSON)
+ {
+     foreach ($objJSON as $k => $obj) {
+         if (is_object($obj)) {
+             $objJSON->$k = objectToArray($obj);
+         } else {
+             $objJSON->$k = $obj;
+         }
+     }
+     return (array) $objJSON;
+ }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//remplacer 0.5 en 1/2
+function remplacerFraction($floNumber){
+  if(preg_match("/^[1-9][0-9]*\.5[0]{0,}$/", $floNumber))
+  {
+      echo $used = preg_replace("/\.5[0]{0,}$/", "&frac12;", $floNumber);
+  }
+  elseif(preg_match("/^[0]*\.5[0]{0,}$/", $floNumber))
+  {
+      echo $used = str_replace("$floNumber", "&frac12;", $floNumber);
+  }
+  else
+  {
+      echo $floNumber;
+  }
+}
+
+
 
 ?>
